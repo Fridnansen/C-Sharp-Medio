db30f5e1153e4a09a5861c8b438fd1b4f5a63148
Francisco José Díaz <74576885+Fridnansen@users.noreply.github.com>
2020-11-28 14:22:31 -0300

    Add files via upload

---
+++ WpfApp74/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp74
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Console.WriteLine("Le has dado al Boton");
+
+            MessageBox.Show("Le has dado al boton 2");
+
+
+        }
+
+       /* private void Panel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Le has dado al Panel");
+        }*/
+
+        private void Panel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            MessageBox.Show("Le has dado al Panel");
+
+        }
+    }
+}
